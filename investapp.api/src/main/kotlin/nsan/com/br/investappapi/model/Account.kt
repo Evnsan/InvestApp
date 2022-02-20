50149e16b38de8c69c66d0ca9a1ff235103fe885
evandro <evandro@cobli.co>
2022-02-19 21:23:05 -0300

    Add bidirectional external relationship to account and agent models

--- investapp.api/src/main/kotlin/nsan/com/br/investappapi/model/Account.kt
+++ investapp.api/src/main/kotlin/nsan/com/br/investappapi/model/Account.kt
@@ -1,9 +1,13 @@
 package nsan.com.br.investappapi.model
 
+import org.springframework.data.jdbc.core.mapping.AggregateReference
+import org.springframework.data.relational.core.conversion.AggregateChange
 import org.springframework.data.relational.core.mapping.Table
+import java.util.UUID
 
 @Table
 data class Account(
     val code: String,
-    val agent_id: Agent) {
+    val agent_id: AggregateReference<Agent, UUID>
+) {
 }
